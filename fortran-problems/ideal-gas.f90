a2671a4617b08a7be5809d2bdaded62346f03ee0
kianhayes <hayesk190@gmail.com>
2023-06-22 10:55:34 -0400

    Testing comments

--- fortran-problems/ideal-gas.f90
+++ fortran-problems/ideal-gas.f90
@@ -2,12 +2,14 @@ program ideal_gas
 
 implicit none
 
+!* Variables
 integer :: pressure
 integer, parameter :: temp = 273
 REAL :: volume
 REAL, parameter :: n = 6.02e23
 REAL, parameter :: r = 8.314
 
+!* Calculates the volume over a range of pressures using the Ideal Gas Law
 DO pressure = 1, 1001, 100
 
     volume = (n * r * temp) / pressure
